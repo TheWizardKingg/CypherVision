dd1a7f5a15f88aa830da93e8bef71d52c38f9146
TheWizardKing <sharmamohit53998@gmail.com>
2026-06-29 00:16:31 +0530

    completing the final CameraManager class

--- cyphervision/core/camera.py
+++ cyphervision/core/camera.py
@@ -1,42 +1,37 @@
 import cv2
-import numpy as np
 
 
 class CameraManager:
 
-    def __init__(self):
-        self.Isopen=False
-        self.camera=None
-        self.resolution=(0, 0)
-        self.FPS=0
+    def __init__(self, source=0):
+        self.Isopen = False
+        self.camera = None
+        self.source = source
+        self.resolution = (0, 0)
+        self.FPS = 0
 
     def open(self):
-        self.camera=cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(self.source)
+
         if not self.camera.isOpened():
             return
-        
-        self.Isopen=True
-        self.resolution=(int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        self.FPS=self.camera.get(cv2.CAP_PROP_FPS)
+
+        self.Isopen = True
+
+        self.resolution = (
+            int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        )
+
+        self.FPS = self.camera.get(cv2.CAP_PROP_FPS)
 
     def read_frame(self):
-        ret, frame=self.camera.read()
+        ret, frame = self.camera.read()
         return ret, frame
 
     def close(self):
         if self.camera is not None:
             self.camera.release()
+
         cv2.destroyAllWindows()
-        self.Isopen=False
-
-Cam1=CameraManager()
-Cam1.open()
-while True:
-    success, frame = Cam1.read_frame()
-    if not success: 
-        break
-    cv2.imshow("window", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-Cam1.close()
-
+        self.Isopen = False
