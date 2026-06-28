38bdd08a2ec3cfdef71b9f40ec1b600e4b80ae3b
TheWizardKing <sharmamohit53998@gmail.com>
2026-06-29 00:30:15 +0530

    Separating the main running loop from the Camera part

---
+++ cyphervision/core/engine.py
@@ -0,0 +1,18 @@
+from cyphervision.core.camera import CameraManager
+import cv2
+
+class Engine:
+    def __init__(self):
+        self.camera=CameraManager()
+    
+    def run(self):
+        while True:
+            success, frame = self.camera.read_frame()
+            if not success:
+                break
+
+            cv2.imshow("window",frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        self.camera.close()
