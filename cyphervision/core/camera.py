97c6642b35abb30157a4c324b94494eba7783a8d
TheWizardKing <sharmamohit53998@gmail.com>
2026-06-28 16:15:49 +0530

    successfully built a CameraManager class which opens, closes the video camera

--- cyphervision/core/camera.py
+++ cyphervision/core/camera.py
@@ -1,8 +1,41 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("assets/input/image.jpg")
-image_resized=cv2.resize(image,(1920,1080))
-cv2.imshow("window",image_resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+class CameraManager:
+
+    def __init__(self):
+        self.Isopen=False
+        self.camera=None
+        self.resolution=(0, 0)
+        self.FPS=0
+
+    def open(self):
+        self.camera=cv2.VideoCapture(0)
+        if not self.camera.isOpened():
+            return
+        
+        self.Isopen=True
+        self.resolution=(int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        self.FPS=self.camera.get(cv2.CAP_PROP_FPS)
+
+        while True:
+            ret, frame = self.camera.read()
+
+            if not ret:
+                break
+
+            cv2.imshow("window", frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+        self.close()
+
+    def close(self):
+        if self.camera is not None:
+            self.camera.release()
+        cv2.destroyAllWindows()
+        self.Isopen=False
+
+Cam1=CameraManager()
+Cam1.open()
