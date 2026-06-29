d91a37c04913c54304a842b752fc6bd7f7924382
TheWizardKing <sharmamohit53998@gmail.com>
2026-06-29 23:08:29 +0530

    completed my CameraManager class, and successfully tested the frame fetch part

--- cyphervision/core/camera.py
+++ cyphervision/core/camera.py
@@ -1,14 +1,14 @@
 import cv2
 class CameraManager:
 
-    def __init__(self):
+    def __init__(self):       #This simply keeps track of all the biginning camera state's data
         self.Isopen = False
         self.camera = None
         self.source = 0
         self.resolution = (0, 0)
         self.FPS = 0
 
-    def open(self):
+    def open(self):                  # This opens the camera
         self.camera = cv2.VideoCapture(self.source)
 
         if not self.camera.isOpened():
@@ -23,26 +23,26 @@ class CameraManager:
 
         self.FPS = self.camera.get(cv2.CAP_PROP_FPS)
 
-    def read_frame(self):
-        ret, frame = self.camera.read()
+    def read_frame(self):                # this thing returns two things, first (ret) is a bool value, whether the given frame is valid or not
+        ret, frame = self.camera.read()      # frame is the actual frame returned by the built-in read() function
         return ret, frame
 
-    def close(self):
+    def close(self):               # This simply closes the camera, freeing up resources
         if self.camera is not None:
             self.camera.release()
 
-        cv2.destroyAllWindows()
+        cv2.destroyAllWindows()          
         self.Isopen = False
 
-    def start(self):
+    def start(self):       # just a method to automate the upper three necessary function
         self.open()
         while True:
-            if not self.read_frame()[0]: 
+            if not self.read_frame()[0]:       # basically the first thing returned by read_frame method (ret [boolean value])
                 break
-            cv2.imshow("window", cv2.cvtColor(self.read_frame()[1],cv2.COLOR_BGR2GRAY))
-            if cv2.waitKey(1)==ord('q'):
-                break
+            cv2.imshow("window", cv2.cvtColor(self.read_frame()[1],cv2.COLOR_BGR2GRAY))      # second thing returned by read_frame (the actual frame)
+            if cv2.waitKey(1)==ord('q'):        # waitKey(...x...), here, x is the time in miliseconds to wait before destroying cv2 windows {0->infinite time}
+                break         # checks, whether after 1 milisecond of showing result, whether user enters a specific key (aka q) to delete all windows
         self.close()
 
-cam1= CameraManager()
-cam1.start()
+cam1= CameraManager()       # making an object
+cam1.start()       # starting the object's work
