3067e1b5388c87085fb86a4ad8ae187f21bc5bd0
TheWizardKing <sharmamohit53998@gmail.com>
2026-07-01 23:31:34 +0530

    Updated the process function to display up finger's tip_ids

--- models/gestureRecognition.py
+++ models/gestureRecognition.py
@@ -1,6 +1,9 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+from pynput.keyboard import Controller
+
+keyboard = Controller()
 
 class GestureRecognizer:
 
@@ -16,6 +19,16 @@ class GestureRecognizer:
 
         self.drawer=mp.solutions.drawing_utils
 
+    def isFingerUp(self, hand, tip_id):
+        if tip_id==4:
+            if hand.landmark[tip_id].x > hand.landmark[tip_id - 2].x:
+                return True
+        else:
+            for i in hand.landmark:
+                if hand.landmark[tip_id].y < hand.landmark[tip_id-2].y:
+                    return True
+
+        
 
     def processThis(self, original_frame):
         rgb_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2RGB)
@@ -23,7 +36,16 @@ class GestureRecognizer:
         results=self.hands.process(rgb_frame)
 
         if results.multi_hand_landmarks:
+            fingers=[]
             for hand in results.multi_hand_landmarks:
+                for i in range(1,6):
+                    if self.isFingerUp(hand, 4*i):
+                        fingers.append(4*i)
+                
+                print(fingers)
+                fingers.clear()
+
+                    
                 self.drawer.draw_landmarks(original_frame, hand, self.mp_hands.HAND_CONNECTIONS)
         
         return original_frame
